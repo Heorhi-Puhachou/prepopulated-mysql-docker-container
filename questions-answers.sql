4287077e09018daabd5e134b69021a6830524a37
Heorhi Puhachou <ce-heorhi.puhachou@beyondnow.com>
2023-06-15 15:28:09 +0200

    Interview preparation

---
+++ questions-answers.sql
@@ -0,0 +1,19 @@
+# Get unique publishers names with copies
+SELECT DISTINCT p.name
+FROM publisher p
+         JOIN game g on p.publisher_id = g.publisher_id;
+
+# Get copies by publisher
+
+SELECT p.name, SUM(g.copies) sum
+FROM publisher p
+         JOIN game g on p.publisher_id = g.publisher_id
+GROUP BY p.name
+HAVING sum > 110000;
+
+# Get copies by publisher if more than 110 000
+SELECT p.name, SUM(g.copies) sum
+FROM publisher p
+         JOIN game g on p.publisher_id = g.publisher_id
+GROUP BY p.name
+HAVING sum > 110000;
--- setup.sql
+++ setup.sql
@@ -2,30 +2,45 @@ CREATE DATABASE myexample;
 
 USE myexample;
 
+CREATE TABLE publisher
+(
+    publisher_id int primary key NOT NULL,
+    name         varchar(255)    NOT NULL,
+    rating       int             NOT NULL
+);
+
+INSERT INTO publisher (publisher_id,
+                       name,
+                       rating)
+VALUES (1,
+        'Rockstar',
+        92),
+       (2,
+        'Nintendo',
+        97);
+
 CREATE TABLE game
 (
     game_id      int AUTO_INCREMENT primary key NOT NULL,
-    publisher_id int          NOT NULL,
-    name         varchar(255) NOT NULL,
+    publisher_id int                            NOT NULL,
+    name         varchar(255)                   NOT NULL,
+    copies       int,
     release_date DATE
 );
 
 INSERT INTO game (name,
                   publisher_id,
+                  copies,
                   release_date)
-VALUES ('Doom',
+VALUES ('GTA',
         1,
+        10000,
         '20190601'),
-       ('GTA',
+       ('Bayonetta',
         2,
+        20000,
         '20191001'),
        ('Legend of Zelda',
         2,
+        100000,
         '20191201');
-
-CREATE TABLE publisher
-(
-    publisher_id int AUTO_INCREMENT primary key NOT NULL,
-    name         varchar(255) NOT NULL,
-    rating       int          NOT NULL
-);
