f0c76d28ce2efa4ae30842d60fcd6ec53cd4f8d7
Heorhi Puhachou <ce-heorhi.puhachou@beyondnow.com>
2023-06-15 15:51:29 +0200

    Small fixes

--- questions-answers.sql
+++ questions-answers.sql
@@ -7,8 +7,7 @@ FROM publisher p
 SELECT p.name, SUM(g.copies) sum
 FROM publisher p
          JOIN game g on p.publisher_id = g.publisher_id
-GROUP BY p.name
-HAVING sum > 110000;
+GROUP BY p.name;
 
 # Get copies by publisher if more than 110 000
 SELECT p.name, SUM(g.copies) sum
